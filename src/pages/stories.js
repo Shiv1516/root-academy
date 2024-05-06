624876b4cdeffe9befb87355721b66019341cfee
Shivnilay Srivastav <shivnilaysrivastav@gmail.com>
2024-05-06 18:15:44 +0530

    file will be updated

--- src/pages/stories.js
+++ src/pages/stories.js
@@ -20,7 +20,21 @@ const stories = () => {
 
       <Testimonial />
 
-      <section></section>
+      <section className="stories ptb40  aft pr">
+        <div className="stories-section wrapper df jcc w100 ptb40">
+          <div className="zi3 tac df fdc aic w60">
+            <h2 className="stories-heading zi3 fc5 fs36 lh42 fw6 mb24">
+              Discover Limitless Opportunities for Your Child Here
+            </h2>
+            <Link
+              href="/contact"
+              className="stories-btn ptb16 plr24 zi3 fc5 brd1 dib br4"
+            >
+              Contact US
+            </Link>
+          </div>
+        </div>
+      </section>
 
       <ParentTestimonial />
     </>
